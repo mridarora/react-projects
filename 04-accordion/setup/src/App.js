046146631dbf848e3ea8018a79cecc76ac9d75ee
mridarora <aroram15@mcmaster.ca>
2021-07-23 18:25:50 +0530

    04 done

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and Answers about login</h3>
+        <div className="info">
+          {questions.map((d) => {
+            return <SingleQuestion key={questions.id} {...d} />
+          })}
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  const [isPlus, setIsPlus] = useState(true);
+
+  return (
+    < article className="question" >
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setIsPlus(!isPlus)}>
+          {isPlus ? <AiOutlinePlus /> : < AiOutlineMinus />}
+        </button>
+      </header>
+      {!isPlus && <p>{info}</p>}
+    </article >
+  );
 };
 
 export default Question;
