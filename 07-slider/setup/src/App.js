db0ceba4a4a8cd20f307e1b2b96156ade1af51ff
mridarora <aroram15@mcmaster.ca>
2021-07-24 21:55:34 +0530

    not doing 07

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,7 +3,15 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  return (
+    <section class="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+    </section>
+  );
 }
 
 export default App;
