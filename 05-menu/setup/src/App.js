1a6ec2967801f5e2dbf7be79938855647eece6e7
mridarora <aroram15@mcmaster.ca>
2021-07-24 14:42:45 +0530

    05 done

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -2,9 +2,30 @@ import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      return setMenuItems(items);
+    }
+    let newMenu = items.filter((item) => item.category === category);
+    setMenuItems(newMenu);
+  }
+
+  return (
+    <main>
+      <section className="menu section">
+        <h2 className="title">Our menu</h2>
+        <div className="underline"></div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ items }) => {
+  return (
+    <div className="section-center">
+      {items.map((singleItem) => {
+        const { title, price, img, desc } = singleItem;
+        return (
+          <div className="menu-item">
+            <img className="photo" src={img} alt={title}></img>
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <p>{price}</p>
+              </header>
+              <p>{desc}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
