cbd696db94d6ac7f6a92739102ed9450718f2bdf
mridarora <aroram15@mcmaster.ca>
2021-07-28 14:44:09 +0530

    test

--- 11-navbar/final/src/Navbar.js
+++ 11-navbar/final/src/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaCentercode } from 'react-icons/fa';
 import { links, social } from './data';
 import logo from './logo.svg';
 
